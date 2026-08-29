36fe05e5af58ccfb7279ab55708d50016f951197
Lucas Eckert <lucasismaeleckert@gmail.com>
2026-08-29 19:27:54 -0300

    style: format users composition root

--- internal/users/module.go
+++ internal/users/module.go
@@ -12,12 +12,24 @@ import (
 	presentationhttp "github.com/Luca5Eckert/trama/internal/users/presentation/http"
 )
 
-type Module struct { controller *presentationhttp.UserController }
+type Module struct {
+	controller *presentationhttp.UserController
+}
 
 func NewModule(pool *pgxpool.Pool) Module {
 	repository := postgres.NewUserRepository(pool)
-	createUser := command.NewCreateUser(repository, system.NewRandomIDGenerator(), system.NewUTCClock())
-	return Module{controller: presentationhttp.NewUserController(createUser, query.NewGetUserByID(repository), query.NewListUsers(repository))}
+	idGenerator := system.NewRandomIDGenerator()
+	clock := system.NewUTCClock()
+
+	createUser := command.NewCreateUser(repository, idGenerator, clock)
+	getUserByID := query.NewGetUserByID(repository)
+	listUsers := query.NewListUsers(repository)
+
+	return Module{
+		controller: presentationhttp.NewUserController(createUser, getUserByID, listUsers),
+	}
 }
 
-func (module Module) RegisterRoutes(mux *http.ServeMux) { module.controller.RegisterRoutes(mux) }
+func (module Module) RegisterRoutes(mux *http.ServeMux) {
+	module.controller.RegisterRoutes(mux)
+}
